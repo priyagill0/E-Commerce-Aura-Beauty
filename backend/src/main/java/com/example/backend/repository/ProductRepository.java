1c3c7933029da6aa4b84c80b189926ee09e740c9
Natalie <156714892+nataliel359@users.noreply.github.com>
2025-11-19 20:21:50 -0500

    Category and Product added

--- backend/src/main/java/com/example/backend/repository/ProductRepository.java
+++ backend/src/main/java/com/example/backend/repository/ProductRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.backend.model.Product;
 
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
+public interface ProductRepository extends JpaRepository<Product, String> {}
